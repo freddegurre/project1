1de12ea4b77028b1f65295d6c405e1838b9454a9
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-03 19:07:12 -0800

    saturday night

--- assets/javascript.js
+++ assets/javascript.js
@@ -22,90 +22,6 @@ function initMap() {
         center: { lat: 40.674, lng: -73.945 },
         zoom: 14,
 
-
-
-        //just to get map black from here!!! 
-        styles: [
-            { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-            {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#d59563' }]
-            },
-            {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#d59563' }]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{ color: '#263c3f' }]
-            },
-            {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#6b9a76' }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{ color: '#38414e' }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{ color: '#212a37' }]
-            },
-            {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#9ca5b3' }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{ color: '#746855' }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{ color: '#1f2835' }]
-            },
-            {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#f3d19c' }]
-            },
-            {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{ color: '#2f3948' }]
-            },
-            {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#d59563' }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{ color: '#17263c' }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{ color: '#515c6d' }]
-            },
-            {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{ color: '#17263c' }]
-            }
-            //to here!!!!
-        ]
     });
     infoWindow = new google.maps.InfoWindow;
 
@@ -117,7 +33,6 @@ function initMap() {
 
     // Adds a marker to the map.
     function addMarker(location, map) {
-        // Add the marker at the clicked location,
 
                 //the query for the emojis. 
                 var queryUrl = "https://www.emojidex.com/api/v1/emoji/";
@@ -154,6 +69,7 @@ function initMap() {
 
         });
         
+        
         //when anything on page with calass .setEmoji is clicked
         $(document).on("click", ".setEmoji", function () {
             //store value in the image var. 
@@ -167,7 +83,6 @@ function initMap() {
                 customInfo: uniqueId
                 
             });*/
-                
         });
         
       
@@ -188,7 +103,7 @@ function initMap() {
        
         //always open popup div
         infowindow.open(map, marker);
-        
+
         $("#bodyContent").empty();
         $(".subHeader").html("<h5>Describe this spot!</h5>");
 
@@ -207,11 +122,6 @@ function initMap() {
         });
 
     }
-
-
-    //$("#")
-
-
 
     console.log(markers)
 

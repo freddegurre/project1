a0053ca86b1b43206ddb50a660aaa0af0d2bca1b
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-05 17:43:12 -0800

    just need to convert string to numbers

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,7 +4,8 @@
 //var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 //var labelIndex = 0;
 var markers = [];
-var map, infoWindow;
+var map;
+var infowindow;
 var infoWindows = [];
 var image;
 var uniqueId = 1;
@@ -25,85 +26,83 @@ function initMap() {
     google.maps.event.addListener(map, 'click', function (event) {
         addMarker(event.latLng, map);
         console.log(event.latLng.lat);
-        map.setCenter(event.latLng); 
+        map.setCenter(event.latLng);
 
     });
 
     // Adds a marker to the map.
-    function addMarker(location, map) {
+    function addMarker(location, map, image, info) {
 
         //-------------
-                //the query for the emojis. 
-                var queryUrl = "https://www.emojidex.com/api/v1/utf_emoji/";
-                var proxy = "https://cors-anywhere.herokuapp.com/";
+        //the query for the emojis. 
+        var queryUrl = "https://www.emojidex.com/api/v1/utf_emoji/";
+        var proxy = "https://cors-anywhere.herokuapp.com/";
 
-                //call the query
-                $.ajax({
-                    method: "GET",
-                    url: proxy + queryUrl
-                }).then(function (response) {
-                    //for each emoji in response
-                    // console.log(response);
-                    var results = JSON.parse(response);
-                    for (i = 0; i < results.length; i++) {
-                    //create div and give it class 
-                        var emojiDiv = $("<div class='setEmoji'>");
-                        
-                        //check if there is actual emoji in the response
-                        if (results[i].moji) {
-                            if(results[i].category === "abstract"){
-                                $(".abstract").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "faces"){
-                                $(".faces").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "food"){
-                                // console.log("yay food ", results[i].moji)
-                                $(".food").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "nature"){
-                                $(".nature").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "objects"){
-                                $(".objects").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "places"){
-                                $(".places").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "symbols"){
-                                $(".symbols").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "tools"){
-                                $(".tools").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
-                            if(results[i].category === "transportation"){
-                                $(".transportation").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                            }
+        //call the query
+        $.ajax({
+            method: "GET",
+            url: proxy + queryUrl
+        }).then(function (response) {
+            //for each emoji in response
+            // console.log(response);
+            var results = JSON.parse(response);
+            for (i = 0; i < results.length; i++) {
+                //create div and give it class 
+                var emojiDiv = $("<div class='setEmoji'>");
 
-                            // if(results[i].category === "food"){
-                            //     console.log("yay food ", results[i].moji)
-                            //     emojiDiv.append(results[i].moji);
-                            // }
-
-                            // if(results[i].category === "food"){
-                            //     console.log("yay food ", results[i].moji)
-                            //     emojiDiv.append(results[i].moji);
-                            // }
-                            //append the emoji to the emojiDiv 
-                            // emojiDiv.append(results[i].moji);
-                            //append the emoji div to body content.
-                            // $(".emojiBox").append(foodDiv);
-                            //set the data attribute equal to emoji 
-                            
-
-
-                        }
-
+                //check if there is actual emoji in the response
+                if (results[i].moji) {
+                    if (results[i].category === "abstract") {
+                        $(".abstract").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
                     }
-                    // $(document).on("click", ".setEmoji", function() {
-                    //     alert("emoji clicked");
-                    // });
-                });
+                    if (results[i].category === "faces") {
+                        $(".faces").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "food") {
+                        // console.log("yay food ", results[i].moji)
+                        $(".food").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "nature") {
+                        $(".nature").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "objects") {
+                        $(".objects").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "places") {
+                        $(".places").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "symbols") {
+                        $(".symbols").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "tools") {
+                        $(".tools").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+                    if (results[i].category === "transportation") {
+                        $(".transportation").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
+                    }
+
+                    // if(results[i].category === "food"){
+                    //     console.log("yay food ", results[i].moji)
+                    //     emojiDiv.append(results[i].moji);
+                    // }
+
+                    // if(results[i].category === "food"){
+                    //     console.log("yay food ", results[i].moji)
+                    //     emojiDiv.append(results[i].moji);
+                    // }
+                    //append the emoji to the emojiDiv 
+                    // emojiDiv.append(results[i].moji);
+                    //append the emoji div to body content.
+                    // $(".emojiBox").append(foodDiv);
+                    //set the data attribute equal to emoji 
+
+
+
+                }
+
+            }
+        
+        });
 
         //---------
 
@@ -118,10 +117,11 @@ function initMap() {
             map: map,
             icon: marker,
             customInfo: uniqueId,
-            info: [],
+            info: []
 
 
         });
+
 
         //when anything on page with calass .setEmoji is clicked
         $(document).on("click", ".setEmoji", function () {
@@ -143,129 +143,152 @@ function initMap() {
         uniqueId++;
         //push the marker to array of markers. 
         markers.push(marker);
-        
-        
+
+
         //this opens popup when marker is clicked. 
         marker.addListener('click', function () {
             this.infowindow.open(map, this);
 
         });
 
-     var contentString = '<div id="content">' +
-        '</div>' +
-        '<br>' +
-        '<div id="namePin">' +
+        var contentString = '<div id="content">' +
+            '</div>' +
+            '<br>' +
+            '<div id="namePin">' +
             '<form class="form-inline">' +
-                '<divclass="input-group">' +
-                    '<input type="text" class="form-control" id="input" placeholder="Location Title">' +
-                 '</div>' +
+            '<divclass="input-group">' +
+            '<input type="text" class="form-control" id="input" placeholder="Location Title">' +
+            '</div>' +
             '</form>' +
-        '</div>' +
-        '<br>' +
-        '<div class="subHeader">'+
-        '</div>'+
-        '<textarea class="form-control" id="description" rows="3"></textarea>'+
-        '<br>' +
-        '<div class="dropdown">' +
-        '<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
+            '</div>' +
+            '<br>' +
+            '<div class="subHeader">' +
+            '</div>' +
+            '<textarea class="form-control" id="description" rows="3"></textarea>' +
+            '<br>' +
+            '<div class="dropdown">' +
+            '<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
             'Emoji Choices' +
             '<span class="caret"></span>' +
-        '</button>' +
-        '<br>' +
-        '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">' +
+            '</button>' +
+            '<br>' +
+            '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">' +
             '<li>' +
-                '<div class="emojiBox">' +
-                '<div class= "abstract">Abstract' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "faces">Faces' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "food">Food' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "nature">Nature' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "objects">Objects' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "places">Places' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "symbols">Symbols' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "tools">Tools' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
-                '<div class= "transportation">Transportation' +
-                '<br>' +
-                '</div>' +
-                '<br>' +
+            '<div class="emojiBox">' +
+            '<div class= "abstract">Abstract' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "faces">Faces' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "food">Food' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "nature">Nature' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "objects">Objects' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "places">Places' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "symbols">Symbols' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "tools">Tools' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
+            '<div class= "transportation">Transportation' +
+            '<br>' +
+            '</div>' +
+            '<br>' +
             '</li>' +
-        '</ul>' +
-        '</div>' +
-        '<div id="bodyContent">' +
-        '</div>' +
-        '<br>' +
-        '<button type="submit" class="btn btn-primary btn-xs" id="pinName"> Save Location </button>'+
-        '<button type="submit" class="btn btn-danger btn-xs" id="delete"> Delete Location </button>';
-       
+            '</ul>' +
+            '</div>' +
+            '<div id="bodyContent">' +
+            '</div>' +
+            '<br>' +
+            '<button type="submit" class="btn btn-primary btn-xs" id="pinName"> Save Location </button>' +
+            '<button type="submit" class="btn btn-danger btn-xs" id="delete"> Delete Location </button>';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 500,
-        // pixelOffset: new google.maps.Size(160,200)
-    });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500,
+            // pixelOffset: new google.maps.Size(160,200)
+        });
 
         //always open popup div when marker is created
         infowindow.open(map, marker);
 
-        marker.infowindow = infowindow  
-        
+        marker.infowindow = infowindow
+
     }
 
- //when save inside infowindow is clicked
- $(document).on("click", "#pinName", function (){
-            event.preventDefault()
-            //store the value that user input in the topic-input form
-            var pinName = $("#input").val().trim();
-            //store the value of description in variable
-            var description = $("#description").val().trim();
-            //hide description form
-            $("#description").hide();
-            //push the name to popup 
-            $("#namePin").html("<h2>" + pinName + "</h2>");
-            //change the subheader to be the actual description written
-            $(".subHeader").html("<h5>" + description + "</h5>");
-            $("#description").hide()
-            $("#pinName").hide()
-   
-          //push the pin name and description to marker info
-            marker.info.push(pinName, description);
-            console.log(pinName);
-            console.log(description);
-            console.log(marker.label);
-            console.log(marker.position);
-            localStorage.setItem('pinName', pinName);
-            localStorage.setItem('description', description);
-            localStorage.setItem('marker.label', marker.label);
-            localStorage.setItem('marker.position', marker.position);
-        }); 
+    //when save inside infowindow is clicked
+    $(document).on("click", "#pinName", function () {
+        event.preventDefault()
+        //store the value that user input in the topic-input form
+        var pinName = $("#input").val().trim();
+        //store the value of description in variable
+        var description = $("#description").val().trim();
+        //hide description form
+        $("#description").hide();
+        //push the name to popup 
+        $("#namePin").html("<h2>" + pinName + "</h2>");
+        //change the subheader to be the actual description written
+        $(".subHeader").html("<h5>" + description + "</h5>");
+        $("#description").hide()
+        $("#pinName").hide()
+        marker.info.push(pinName, description);
 
-   }
-     
-      
-    
+        
+        localStorage.setItem('pinName', pinName);
+        localStorage.setItem('description', description);
+        localStorage.setItem('marker.label', marker.label);
+        localStorage.setItem('marker.position', marker.position);
+         
+    });
+
+  
+    var savedName = localStorage.getItem('pinName');
+  console.log(savedName);
+  var savedDesc = localStorage.getItem('description');
+  console.log(savedDesc);
+  var savedLabel = localStorage.getItem('marker.label');
+  console.log(savedLabel);
+  var savedPosition = localStorage.getItem('marker.position');
+  savedPosition= savedPosition.replace('(','');
+  savedPosition= savedPosition.replace(')','');
+  savedPosition = savedPosition.split(",")
+
+  parseInt(savedPosition[0])
+  parseInt(savedPosition[1])
+
+  
+ savedPosition = {lat:savedPosition[0], lng:savedPosition[1]};
+
+ console.log(savedPosition);
+
+
+ //push the pin name and description to marker info
+
+ 
+addMarker(savedPosition, map, savedLabel, savedName, savedDesc)
+}
+//{lat:cord[0], lng:cord[1]};
+
+
+
 
  // this deletes one marker. we need to change the event listener
        // google.maps.event.addListener(marker, 'click', function (event) {
@@ -303,4 +326,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
-}
+}*/

0024158df5854e6647039b45efafc661c3a4e164
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-03-07 00:28:51 -0800

    now multiple infowindows can be open but you will only affect content of one, however i do not recoment it

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,6 @@
 
 //google maps api key AIzaSyD7b6YGD2VKhvICqzlYp3rvpn-V54UMP3Y
 
-//var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//var labelIndex = 0;
 var markers = [];
 var map;
 var infowindow;
@@ -11,10 +9,11 @@ var image;
 var uniqueId = 0;
 var marker;
 var deleteID = 0;
+var saveID = 0;
 
 function initMap() {
 
-	
+
     // Styles a map in night mode.
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.674, lng: -73.945 },
@@ -22,7 +21,7 @@ function initMap() {
 
     });
 
-	
+
     // This event listener calls addMarker() when the map is clicked.
     google.maps.event.addListener(map, 'click', function (event) {
         addMarker(event.latLng, map);
@@ -30,10 +29,10 @@ function initMap() {
         //map.setCenter(event.latLng);
 
     });
+
     // Adds a marker to the map.
     function addMarker(location, map, image, info) {
 
-        //-------------    
 
         //the query for the emojis. 
         var queryUrl = "https://www.emojidex.com/api/v1/utf_emoji/";
@@ -53,12 +52,7 @@ function initMap() {
 
                 //check if there is actual emoji in the response
                 if (results[i].moji) {
-                    if (results[i].category === "abstract") {
-                        $(".abstract").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                    }
-                    if (results[i].category === "faces") {
-                        $(".faces").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                    }
+
                     if (results[i].category === "food") {
                         // console.log("yay food ", results[i].moji)
                         $(".food").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
@@ -72,9 +66,6 @@ function initMap() {
                     if (results[i].category === "places") {
                         $(".places").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
                     }
-                    if (results[i].category === "symbols") {
-                        $(".symbols").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
-                    }
                     if (results[i].category === "tools") {
                         $(".tools").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
                     }
@@ -82,33 +73,24 @@ function initMap() {
                         $(".transportation").append("<div class='setEmoji' data-emoji='" + results[i].moji + "'>" + results[i].moji);
                     }
 
-
-
-
                 }
 
             }
 
         });
 
-        //---------
-
         //create a new marker. 
         marker = new google.maps.Marker({
             animation: google.maps.Animation.DROP,
             position: location,
-            //label: image,
             label: {
-                fontSize: "30px",
                 text: image,
-                
+                fontSize: "30px",
             },
             map: map,
-            icon: 'https://i.imgur.com/CoiFeTy.png',
+            icon: 'https://i.imgur.com/qnkW5nH.png',
             customInfo: uniqueId,
             info: []
-
-
         });
 
 
@@ -116,10 +98,7 @@ function initMap() {
         $(document).on("click", ".setEmoji", function () {
             //store value in the image var. 
             image = $(this).attr("data-emoji");
-            $(this).css({
-                fontSize: "30px"
-            });
-             
+
             //function to update label of current marker label
             function updateLabel() {
                 // set the label to be chosen emoji
@@ -128,14 +107,15 @@ function initMap() {
                 image = "";
 
             }
+
             updateLabel();
         });
+
 
         // add one to unique id so that next marker get its own id.
         uniqueId++;
         //push the marker to array of markers. 
         markers.push(marker);
-
 
         //this opens popup when marker is clicked. 
         marker.addListener('click', function () {
@@ -152,7 +132,7 @@ function initMap() {
             '</form>' +
             '</div>' +
             '<br>' +
-            '<div class="subHeader">' +
+            '<div id="subHeader">' +
             '</div>' +
             '<textarea class="form-control" id="description" placeholder="Notes" rows="3"></textarea>' +
             '<br>' +
@@ -165,11 +145,11 @@ function initMap() {
             '<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">' +
             '<li>' +
             '<div class="emojiBox">' +
-            '<div class= "abstract">Abstract' +
+            '<div class= "places">Places' +
             '<br>' +
             '</div>' +
             '<br>' +
-            '<div class= "faces">Faces' +
+            '<div class= "transportation">Transportation' +
             '<br>' +
             '</div>' +
             '<br>' +
@@ -185,40 +165,27 @@ function initMap() {
             '<br>' +
             '</div>' +
             '<br>' +
-            '<div class= "places">Places' +
-            '<br>' +
-            '</div>' +
-            '<br>' +
-            '<div class= "symbols">Symbols' +
-            '<br>' +
-            '</div>' +
-            '<br>' +
             '<div class= "tools">Tools' +
             '<br>' +
             '</div>' +
-            '<br>' +
-            '<div class= "transportation">Transportation' +
-            '<br>' +
-            '</div>' +
-            '<br>' +
             '</li>' +
             '</ul>' +
             '</div>' +
             '<div id="bodyContent">' +
             '</div>' +
             '<br>' +
-            '<button type="submit" class="btn btn-primary btn-xs" id="pinName"> Save</button>' +
-            '<button type="submit" class="btn btn-danger btn-xs" id="delete"> Delete</button>';
-           
-
+            '<div id="buttons">' +
+            '<button type="submit" class="save btn btn-primary btn-xs" data= "' + saveID + '"> Save</button>' +
+            '</div>'
 
 
         var infowindow = new google.maps.InfoWindow({
             content: contentString,
             maxWidth: 300,
-            
+
         });
 
+        saveID++;
         //always open popup div when marker is created
         infowindow.open(map, marker);
 
@@ -230,10 +197,10 @@ function initMap() {
 
     }
 
-
     //when save inside infowindow is clicked
-    $(document).on("click", "#pinName", function () {
+    $(document).on("click", ".save", function () {
         event.preventDefault()
+
         //store the value that user input in the topic-input form
         var pinName = $("#input").val().trim();
         //store the value of description in variable
@@ -243,29 +210,81 @@ function initMap() {
         //push the name to popup 
         $("#namePin").html("<h2>" + pinName + "</h2>");
         //change the subheader to be the actual description written
-        $(".subHeader").html("<h5>" + description + "</h5>");
+        $("#subHeader").html("<h5>" + description + "</h5>");
         $("#description").hide()
-        $("#pinName").hide()
-        $("#delete").attr("data", deleteID);
+        $("#buttons").html('<button type="submit" class="delete btn btn-danger btn-xs"> Delete</button>');
+        $(".delete").attr("data", deleteID);
         deleteID++;
         marker.info.push(pinName, description);
+        // eq(indexS)
 
-        
-        //Push item to local storage
-        localStorage.setItem('pinName', pinName);
+    });
+
+
+}
+
+//delete marker function 
+$(document).on("click", ".delete", function () {
+    event.preventDefault()
+
+    // get this marker, then find it in array and take it away from array. 
+    var index = $(this).attr("data");
+    markers[index].setMap(null)
+    console.log(markers);
+//hey
+
+});
+
+
+    //---------- GEO LOCATION 
+
+    // Try HTML5 geolocation.
+if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+
+
+
+
+
+
+//------------CODE Grave yard LOCAL STORAGE
+
+    //Push item to local storage
+       /* localStorage.setItem('pinName', pinName);
         localStorage.setItem('description', description);
         localStorage.setItem('marker.label', marker.label);
-        localStorage.setItem('marker.position', marker.position);
+        localStorage.setItem('marker.position', marker.position);*/
 
         //localStorage.setItem('marker', pinName + description + marker.label + marker.position);
 
         // localStorage.setItem('markers', markers);
 
 
-    });
-
-    //get items from local storage
-    var savedName = localStorage.getItem('pinName');
+        //get items from local storage
+   /* var savedName = localStorage.getItem('pinName');
     console.log(savedName);
     var savedDesc = localStorage.getItem('description');
     console.log(savedDesc);
@@ -286,54 +305,4 @@ function initMap() {
     console.log(localStorage)
 
     //call function to create marker from local storage
-    //addMarker(savedPosition, map, savedLabel, savedName, savedDesc)
-}
-
-//delete marker function 
-$(document).on("click", "#delete", function () {
-    event.preventDefault()
-
-    // get this marker, then find it in array and take it away from array. 
-    var index = $(this).attr("data");
-    markers[index].setMap(null)
-    console.log(markers);
-
-
-});
-
-
-    //---------- GEO LOCATION 
-
-    // Try HTML5 geolocation.
-   /* if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Curret location.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-}
-
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
-google.maps.event.addListener(marker,"click",function(){
-        if(infowindow)infowindow.close(this.InfoWindow);
-    });*/
+    //addMarker(savedPosition, map, savedLabel, savedName, savedDesc)*/
